943dcad77ff6604805eae28df3628f85b02f3d31
Nick Lawrence <39334884+nick-lawrence@users.noreply.github.com>
2019-01-22 09:01:57 -0800

    Update RSLC.py

--- RSLC.py
+++ RSLC.py
@@ -2,17 +2,23 @@
 # Please feel free to use and modify this code as needed
 # License: MIT 
 
+# CONFIG -------------------------------------------------------
+
+# There are 30 labels per sheet on Avery 5660 label paper.
+# Please feel free to modify as needed.
+
+labels_per_sheet = 30
+
+# --------------------------------------------------------------
+
 import math
 
 while True:
   # This takes in how many individual labels you need.
   number_needed = float(input("How many labels do you need? >> "))
-
-  # There are 30 labels per sheet as of now, please feel free to modify as needed.
-  labels_per_sheet = 30.00
   
   #This stores the sheet count by dividing labels needed by the labels per sheet and rounding up so you don't come up short.
-  sheets_needed = math.ceil(number_needed / labels_per_sheet)
+  sheets_needed = math.ceil(number_needed / float(labels_per_sheet))
   
   # This tells the user how many sheets of labels they will need
   print(f">> You will need {sheets_needed} sheets.")
